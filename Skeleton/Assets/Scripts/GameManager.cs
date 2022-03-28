345ac51ca62d4cbcc2eb6870bf462bb814e637ca
Matt Wildman <mjwildman@wisc.edu>
2022-03-27 22:48:30 -0500

    Reading Json successfully

--- Skeleton/Assets/Scripts/GameManager.cs
+++ Skeleton/Assets/Scripts/GameManager.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+
+[Serializable]
+public class FoodData
+{
+    public FoodType[] FoodTypes;
+}
+
+[Serializable]
+public class FoodType
+{
+    public string name;
+    public RecommendedDietInfo recommended;
+    public FoodItem[] items;
+}
+
+[Serializable]
+public class FoodItem
+{
+    public string name;
+    public float price;
+    public string units;
+    public float nutritionPerUnit;
+}
+
+[Serializable]
+public class RecommendedDietInfo
+{
+    public string unit;
+    public int[] women;
+    public int[] men;
+}
+
+public class DailyFoodItem
+{
+    FoodItem foodItem;
+    int minPurchase;
+    int maxPurchase;
+}
 
 public class GameManager : MonoBehaviour
 {
+
     public static GameManager instance;
     public static GameManager get()
     {
         return instance;
     }
+    public TextAsset foodDataJson;
+    FoodData foodData;
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        foodData = UnityEngine.JsonUtility.FromJson<FoodData>(foodDataJson.text);
+        Debug.Log(foodData.FoodTypes[0].items.Length);
     }
 
     // Update is called once per frame
@@ -25,4 +69,9 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log(str);
     }
+
+    //public DailyFoodItem[] GetDailyFoodItems()
+    //{
+
+    //}
 }
